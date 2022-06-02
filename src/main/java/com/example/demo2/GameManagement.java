fd82c38a32392423b6c330591d4fe138e46286e4
mhmtgks <goksumehmetgs@gmail.com>
2022-06-02 11:24:19 +0300

    1

--- src/main/java/com/example/demo2/GameManagement.java
+++ src/main/java/com/example/demo2/GameManagement.java
@@ -8,9 +8,7 @@ import java.util.Random;
 public class GameManagement extends DataManagement {
     public static Random rand = new Random();
     String words="";
-    int puan =0;
-    byte hatakodu=0; // sözlükte yok 1, merkez yok 2 ,4ten az harf 3 ,farklı harf 4 , zaten bulundu 5 , hata kodu 6
-    ArrayList<String> founded = new ArrayList<>();
+    byte hatakodu=0;
 
     public GameManagement() throws FileNotFoundException,IOException {
         super();
@@ -39,53 +37,11 @@ public class GameManagement extends DataManagement {
 
 
 
-    public boolean wordController(String word){
-        String center = words.substring(0,1);
-        boolean bool = false;
-        if(word.contains(center)&&dictionary.contains(word)){
-            bool=true;
-        }//doğru
-        else if(word.contains(center)&& !dictionary.contains(word)){
-            hatakodu=1;
-            bool=false;}
-        else if(!word.contains(center)){
-            hatakodu=2;}//sözlükte yok uyarısı
-        return bool;}
 
 
-    public void pointCalculator(String Word){
-            if(wordController(Word)){
-                founded.add(Word);
-                puan= puan+(Word.length()-3);
-                if(super.isPangram(Word)){
-                    puan=puan+7;
-                }
 
-        }
-    }
-    public void gameController(String Word) {//kelimedeki harfin doğrulukları ekle int dönder hata kodunu
-        hatakodu=0;
-        if(!founded.contains(Word)){
-            if (wordController(Word)){
-                pointCalculator(Word);
-            }else {
-                switch (hatakodu){
-                    case 1:
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
 
-                }
 
-            }
-        }else{
-            //5 firlat
-
-        }
-
-    }
 }
 
 
